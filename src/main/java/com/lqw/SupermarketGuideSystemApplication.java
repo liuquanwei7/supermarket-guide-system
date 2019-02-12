83ac9c05979857a1f6ef3a22e76abe318f6d48e1
liuquanwei <1353965910@qq.com>
2019-02-12 16:22:33 +0800

    Initial commit

---
+++ src/main/java/com/lqw/SupermarketGuideSystemApplication.java
@@ -0,0 +1,14 @@
+package com.lqw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketGuideSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketGuideSystemApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/lqw/SupermarketGuideSystemApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lqw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupermarketGuideSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
